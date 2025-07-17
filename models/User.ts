7759760d8fa013845f7858b5263867f0773e854b
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2025-07-17 12:11:41 +0530

    Update User.ts

--- models/User.ts
+++ models/User.ts
@@ -1,9 +1,8 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose from 'mongoose';
 
-const userSchema = new Schema({
-  name: { type: String },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
 
 export const User = mongoose.models.User || mongoose.model('User', userSchema);
